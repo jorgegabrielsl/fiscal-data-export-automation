79ad96f4b45842ffaf4ebf5f61e885a00819f0be
Jorge Gabriel <105565673+jorgegabrielsl@users.noreply.github.com>
2026-03-04 22:00:51 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+DB_CONFIG = {
+    "database": "example_database",
+    "host": "localhost",
+    "user": "postgres",
+    "password": "password",
+    "port": "5432"
+}
+
+EXPORT_PATH = "./exports/"
